98da859a4e856629546d12ddd4f7ad4ba9b34ef8
KiberDron <andreynotebook@gmail.com>
2024-03-16 04:06:27 +0300

    add then

--- client/src/pages/AdminPage.js
+++ client/src/pages/AdminPage.js
@@ -5,8 +5,8 @@ const AdminPage = () => {
   return (
     <div style={{width: "100%", height: "100%", display: "flex", alignItems: "center"}}>
       <div style={{width: "100%", display: "flex", justifyContent: "center", alignItems: "center"}}>
-        <button onClick={createGreenBall()}>Добавить зеленый шар</button>
-        <button onClick={deleteGreenBalls()}>Удалить все</button>
+        <button onClick={createGreenBall().then((data) => data)}>Добавить зеленый шар</button>
+        <button onClick={deleteGreenBalls().then((data) => data)}>Удалить все</button>
       </div>
     </div>
   );
